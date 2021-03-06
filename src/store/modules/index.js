a1420c5a7327b2f3892b3797b824f03960f4416c
atjhoendz <achunarmando@gmail.com>
2021-03-06 18:19:47 +0700

    chore(state): add auth toat and user

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,5 +1,11 @@
 import dashboard from './dashboard';
+import auth from './auth';
+import toast from './toast';
+import user from './user';
 
 export default {
   dashboard,
+  auth,
+  toast,
+  user,
 };
