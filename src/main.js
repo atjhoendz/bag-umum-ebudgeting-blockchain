fd9d7ff159c2ab295c365453fcae81da87ccc59a
atjhoendz <achunarmando@gmail.com>
2021-03-06 15:07:26 +0700

    chore(main): add dependencies

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import 'core-js/stable'
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import CoreuiVue from '@coreui/vue'
-import { iconsSet as icons } from './assets/icons/icons.js'
-import store from './store'
+import 'core-js/stable';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import CoreuiVue from '@coreui/vue';
+import { iconsSet as icons } from './assets/icons/icons.js';
+import store from './store';
+import Vuelidate from 'vuelidate';
 
-Vue.config.performance = true
-Vue.use(CoreuiVue)
-Vue.prototype.$log = console.log.bind(console)
+Vue.config.performance = true;
+Vue.use(CoreuiVue);
+Vue.use(Vuelidate);
+Vue.prototype.$log = console.log.bind(console);
 
 new Vue({
   el: '#app',
@@ -17,6 +19,6 @@ new Vue({
   icons,
   template: '<App/>',
   components: {
-    App
-  }
-})
+    App,
+  },
+});
